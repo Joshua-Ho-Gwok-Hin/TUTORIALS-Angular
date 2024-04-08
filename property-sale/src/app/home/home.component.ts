71b1db46efa594fa187584d229da9bf51d503515
Joshua Ho Gwok Hin <hgh.joshua@gmail.com>
2024-04-08 19:37:08 +0800

    Added new project property-sale

---
+++ property-sale/src/app/home/home.component.ts
@@ -0,0 +1,27 @@
+import { Component, inject } from '@angular/core';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { CommonModule } from '@angular/common';
+import { HousingService } from '../housing.service';
+import { Housinglocation } from '../housinglocation';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    CommonModule,
+    HousingLocationComponent,
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+})
+export class HomeComponent {
+
+  housingLocationList: Housinglocation[] = [];
+  housingService: HousingService = inject(HousingService);
+
+  constructor(){
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+  }
+  
+  
+}
